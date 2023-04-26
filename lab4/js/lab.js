0a2c1b9a11940242a9fe38364462bc715c17b36d
amaayamichelle <130080079+amaayamichelle@users.noreply.github.com>
2023-04-25 17:39:31 -0700

    added links to the html

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Dots and Boxes: An implemenation of the game
+// Amaya Garcia and Ellie Delyons ammigarc@ucsc.edu
+// April 2024
+
+// print the board setup of dots with the same amount in width and length
+    //players of 2 switch back and forth connecting dots with lines, filling the box with the initial of the person who makes the last line
+      //accept user input
+          //validate user move
+            //test win or lose based on who has the most boxes with their initial
+
 
 // Constants
 
