132a6861fc5ef09f0b7e52119a8fafef41eb6d9b
amaayamichelle <130080079+amaayamichelle@users.noreply.github.com>
2023-05-12 09:02:06 -0700

    change

---
+++ lab 9/lab.js
@@ -0,0 +1,12 @@
+/*
+* Author: Amaya Garcia & Ellie DeLyons
+* Created: 11 May
+* License: Public Domain
+*/
+
+var getElementbyId = document.createElement("outputEl");
+var new1El = document.createElement("p");
+new1El.innerHTML = "We are coding geniuses!";
+var new2El = document.createElement("p");
+new2El.style.fontSize = "31px";
+new1El.appendChild(new2El);
