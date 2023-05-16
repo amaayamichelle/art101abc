cb79a700212f0ef473f53a6962faa7db0545c98f
amaayamichelle <130080079+amaayamichelle@users.noreply.github.com>
2023-05-16 09:55:42 -0700

    change
    changes

---
+++ lab11/lab.js
@@ -0,0 +1,16 @@
+// index.js - Lab 11: Libraries & jQuery 
+// Author: ELlie Delyons & Amaya Garcia
+// Date: May 16, 2023
+
+        <input type="button" value="Button" />;
+        $("button").click(function) {
+            $("h1").remove();
+            })
+        <input type="button" value="Button" />;
+        $("button").click(function) {
+            $("h2").remove();
+            })
+        <input type="button" value="Button" />;
+        $("button").click(function) {
+            $("h3").remove();
+            })
