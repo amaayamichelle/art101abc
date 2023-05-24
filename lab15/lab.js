c864a1db40731031f69e6560169ac5ba8d7475d7
amaayamichelle <130080079+amaayamichelle@users.noreply.github.com>
2023-05-23 20:13:34 -0700

    changes
    changes

---
+++ lab15/lab.js
@@ -0,0 +1,3 @@
+// index.js - Lab 15: AJAX
+// Author: ELlie Delyons & Amaya Garcia
+// Date: June 1, 2023
---
+++ lab16/lab.js
@@ -0,0 +1,3 @@
+// index.js - Lab 16: JSON and APIs
+// Author: ELlie Delyons & Amaya Garcia
+// Date: June 6, 2023
